1a7838a223556c5a2aa825521d343b6ce4af3bba
SrinS <sanja_rindzova@hotmail.com>
2015-04-27 20:58:34 +0200

    Added data base project.

---
+++ RegisterBike/RegisterBike/TestProject.BikeDB/dbo/Tables/Bike.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Bike] (
+    [Id]               INT            IDENTITY (1, 1) NOT NULL,
+    [Model]            NVARCHAR (200) NULL,
+    [Tip]              NVARCHAR (200) NULL,
+    [SeriskiBroj]      NVARCHAR (200) NULL,
+    [Grad]             NVARCHAR (200) NULL,
+    [Flag]             NVARCHAR (200) NULL,
+    [BrojNaBrizini]    INT            NULL,
+    [Boja1]            NVARCHAR (200) NULL,
+    [Boja2]            NVARCHAR (200) NULL,
+    [MaterijalNaRamka] NVARCHAR (200) NULL,
+    CONSTRAINT [PK_Bike] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
